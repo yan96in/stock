44470ddba7cf92484e1d893ed508ecc138ec2bd7
yan96in <yan96in@yan96in-PC>
2018-03-06 14:59:58 +0800

    newsbean+ispider

---
+++ stock/src/main/java/com/yan96in/stock/news/bean/NewsBean.java
@@ -0,0 +1,20 @@
+package com.yan96in.stock.news.bean;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.yan96in.stock.trading.bean.StockBean;
+@Component
+public class NewsBean {
+	//新闻id
+	private String newsId;
+	//新闻标题
+	private String newsTitle;
+	//新闻来源
+	private String newsSource;
+	//相关股票
+	private List<StockBean> relativeStocks;
+	//新闻内容
+	private String newsContent;
+}
--- stock/src/main/java/com/yan96in/stock/util/spider/ISpider.java
+++ stock/src/main/java/com/yan96in/stock/util/spider/ISpider.java
@@ -1,5 +1,9 @@
 package com.yan96in.stock.util.spider;
 
 public interface ISpider {
-
+	//抓取信息来源
+	String infoSource="";
+	//抓取方式
+	//抓取时间-定时抓取
+	
 }
